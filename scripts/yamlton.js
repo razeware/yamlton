6cff7712cc44e7b977d30af390a4824ce4f7215f
Sam Davies <sam@razeware.com>
2020-09-27 00:01:22 +0100

    Allowing hiding of array elements

--- scripts/yamlton.js
+++ scripts/yamlton.js
@@ -1,3 +1,13 @@
+const enableCollapsingArrays = (element) => {
+  const arrayKeys = element.querySelectorAll('.jh-array-key');
+  console.log(arrayKeys);
+  [...arrayKeys].forEach((arrayKey) => {
+    arrayKey.addEventListener('click', () => {
+      arrayKey.nextSibling.classList.toggle('u-hide');
+    });
+  });
+};
+
 document.addEventListener('DOMContentLoaded', () => {
 	const convertButton = document.querySelector('button#convert');
   const yamlTextArea = document.querySelector('textarea#yaml');
@@ -7,5 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const object = jsyaml.load(yaml);
     const table = JsonHuman.format(object);
     output.innerHTML = table.outerHTML;
+    enableCollapsingArrays(output);
   });
 });
