d3962f9eebee44ef13257a13321fc6da6c501ec6
Sam Davies <sam@kodeco.com>
2023-12-02 11:58:53 +0000

    Refactoring the JavaScript

--- scripts/yamlton.js
+++ scripts/yamlton.js
@@ -1,31 +1,44 @@
-const enableCollapsingArrays = (element) => {
-  const arrayKeys = element.querySelectorAll('.jh-array-key');
-  console.log(arrayKeys);
-  [...arrayKeys].forEach((arrayKey) => {
-    arrayKey.addEventListener('click', () => {
-      arrayKey.nextSibling.classList.toggle('u-hide');
+const configureQuiz = () => {
+  const quizHeaders = ['Question', 'A', 'B', 'C', 'D', 'Answer', 'Explanation'];
+
+  const enableQuizForInput = (input) => {
+    // input is a bool, if true, reset the quiz, and hide the output elements.
+    //                  if false, hide the input elements, and show the output elements.
+
+    if(input) {
+      // Find all the quiz reset elements and empty them. Identified by the data-quiz-reset attribute.
+      const quizResetElements = document.querySelectorAll('[data-quiz-reset]');
+      [...quizResetElements].forEach((element) => {
+        element.innerHTML = '';
+        element.value = '';
+      });
+    }
+
+    // Handle result and input elements
+    const quizOutputElements = document.querySelectorAll('[data-quiz-mode="result"]');
+    [...quizOutputElements].forEach((element) => {
+      input ? element.classList.add('is-hidden') : element.classList.remove('is-hidden');
     });
-  });
-};
+    const quizInputElements = document.querySelectorAll('[data-quiz-mode="input"]');
+    [...quizInputElements].forEach((element) => {
+      input ? element.classList.remove('is-hidden') : element.classList.add('is-hidden');
+    });
+  };
 
-const quizHeaders = ['Question', 'A', 'B', 'C', 'D', 'Answer', 'Explanation'];
+  const quizTsvToObject = (tsv) => {
+    const lines = tsv.split(/\n(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+    const objects = lines.map(line => {
+      const data = line.split('\t');
+      return quizHeaders.reduce((obj, header, index) => {
+        obj[header] = data[index].replace(/^"|"$/g, '').replace(/""/g, '"');
+        return obj;
+      }, {});
+    });
 
-const quizTsvToObject = (tsv) => {
-  const lines = tsv.split(/\n(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-  const objects = lines.map(line => {
-    const data = line.split('\t');
-    return quizHeaders.reduce((obj, header, index) => {
-      obj[header] = data[index].replace(/^"|"$/g, '').replace(/""/g, '"');
-      return obj;
-    }, {});
-  });
+    return objects;
+  };
 
-  return objects;
-};
-
-const createHTMLTable = (array) => {
-  if (document.getElementById('quiz-table')) {
-    const tableElement = document.getElementById('quiz-table');
+  const createHTMLTable = (array) => {
     let tableHTML = '<table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">';
     tableHTML += '<thead><tr>';
     quizHeaders.forEach((header) => {
@@ -40,104 +53,98 @@ const createHTMLTable = (array) => {
       tableHTML += '</tr>';
     });
     tableHTML += '</tbody></table>';
-    tableElement.innerHTML = tableHTML;
-  }
-};
 
-
-const convertQuizToYamlFormat = (questions) => {
-  return questions.map(question => {
-    return {
-      question_md: question.Question,
-      choices: [
-        { ref: 'a', option_md: question.A, correct: question.Answer === 'A' },
-        { ref: 'b', option_md: question.B, correct: question.Answer === 'B' },
-        { ref: 'c', option_md: question.C, correct: question.Answer === 'C' },
-        { ref: 'd', option_md: question.D, correct: question.Answer === 'D' }
-      ],
-      explanation_md: question.Explanation,
-      learning_objective: ''
-    }
-  });
-}
-
-const handleQuizPaste = (event) => {
-  // Prevent the default paste action
-  event.preventDefault();
-
-  // Get the pasted data from the clipboard
-  const text = (event.clipboardData || window.clipboardData).getData('text');
-
-  // Put the reformatted text into the quiz yaml box
-  const quizYamlDiv = document.getElementById('quiz-yaml');
-  quizYamlDiv.innerHTML = `<pre><code>${reformatTsvText(text)}</code></pre>`;
-
-  // Hide the quiz source container
-  const quizSourceContainer = document.getElementById('quiz-source-container');
-  quizSourceContainer.classList.add('is-hidden');
-
-  // Show the quiz table
-  const quizTable = document.getElementById('quiz-table');
-  quizTable.classList.remove('is-hidden');
-
-  // Show the reset button
-  const resetButton = document.getElementById('reset');
-  resetButton.classList.remove('is-hidden');
-
-  // Show the yaml container
-  const quizYamlContainer = document.getElementById('quiz-yaml-container');
-  quizYamlContainer.classList.remove('is-hidden');
-};
-
-const reformatTsvText = (text) => {
-  // Convert the pasted data from TSV to a JavaScript object
-  const object =  quizTsvToObject(text);
-
-  // Pop the quiz into a table
-  createHTMLTable(object);
-
-  // Convert to the YAML formatted object
-  const questions = convertQuizToYamlFormat(object);
-
-  // Wrap the questions in a quiz object
-  const quiz = {
-    title: "Quiz Title",
-    assessment_type: "quiz",
-    description_md: "TODO.  This can be multi-line (as long as `|` is used and indendation followed), and uses markdown. Must be a string.",
-    questions: questions
+    return tableHTML;
   };
 
-  // Convert to YAML, using double-quotes for strings
-  return jsyaml.dump(quiz, { quotingType: '"' });
+
+  const convertQuizToYamlFormat = (questions) => {
+    return questions.map(question => {
+      return {
+        question_md: question.Question,
+        choices: [
+          { ref: 'a', option_md: question.A, correct: question.Answer === 'A' },
+          { ref: 'b', option_md: question.B, correct: question.Answer === 'B' },
+          { ref: 'c', option_md: question.C, correct: question.Answer === 'C' },
+          { ref: 'd', option_md: question.D, correct: question.Answer === 'D' }
+        ],
+        explanation_md: question.Explanation,
+        learning_objective: ''
+      }
+    });
+  };
+
+  const convertQuizToYaml = (quizObject) => {
+    // Convert to the YAML formatted object
+    const questions = convertQuizToYamlFormat(quizObject);
+
+    // Wrap the questions in a quiz object
+    const quiz = {
+      title: "Quiz Title",
+      assessment_type: "quiz",
+      description_md: "TODO.  This can be multi-line (as long as `|` is used and indendation followed), and uses markdown. Must be a string.",
+      questions: questions
+    };
+
+    // Convert to YAML, using double-quotes for strings
+    return jsyaml.dump(quiz, { quotingType: '"' });
+  };
+
+  const handleQuizPaste = (event) => {
+    // Prevent the default paste action
+    event.preventDefault();
+
+    // Get the pasted data from the clipboard
+    const text = (event.clipboardData || window.clipboardData).getData('text');
+
+    // Convert imported as TSV as an object
+    const quizObject = quizTsvToObject(text);
+
+    // Pop the quiz into a table
+    const tableElement = document.getElementById('quiz-table');
+    const quizHtml = createHTMLTable(quizObject);
+    tableElement.innerHTML = quizHtml;
+
+    // Put the reformatted text into the quiz yaml box
+    const quizYamlDiv = document.getElementById('quiz-yaml');
+    const yamlText = convertQuizToYaml(quizObject);
+    quizYamlDiv.innerHTML = `<pre><code>${yamlText}</code></pre>`;
+
+    // Switch from input to output
+    enableQuizForInput(false);
+  };
+
+  const resetQuiz = () => {
+    // Switch from output to input
+    enableQuizForInput(true);
+    // Focus on the quiz source text area
+    document.querySelector('textarea#quiz-source')?.focus();
+  }
+
+  // Find the quiz-csv text area and add a listener for pasting
+  const quizSourceTextArea = document.querySelector('textarea#quiz-source');
+  quizSourceTextArea?.addEventListener('paste', handleQuizPaste);
+
+  // Find the reset button and add a listener
+  const resetButton = document.getElementById('reset');
+  resetButton?.addEventListener('click', resetQuiz);
+
+  // Prepare for input
+  enableQuizForInput(true);
 };
 
 
-const resetQuiz = () => {
-  const quizTable = document.getElementById('quiz-table');
-  quizTable.innerHTML = '';
-  const quizSourceTextArea = document.querySelector('textarea#quiz-source');
-  quizSourceTextArea.value = '';
-  // Hide the reset button
-  const resetButton = document.getElementById('reset');
-  resetButton.classList.add('is-hidden');
-  // Hide the quiz table
-  quizTable.classList.add('is-hidden');
-  // Show the quiz source container
-  const quizSourceContainer = document.getElementById('quiz-source-container');
-  quizSourceContainer.classList.remove('is-hidden');
-  // Clear the quiz yaml div
-  const quizYamlDiv = document.getElementById('quiz-yaml');
-  quizYamlDiv.innerHTML = '';
-  // Hide the quiz yaml container div
-  const quizYamlContainer = document.getElementById('quiz-yaml-container');
-  quizYamlContainer.classList.add('is-hidden');
-  // Focus on the quiz source text area
-  quizSourceTextArea.focus();
-}
+const configureTableDisplay = () => {
+  const enableCollapsingArrays = (element) => {
+    const arrayKeys = element.querySelectorAll('.jh-array-key');
+    [...arrayKeys].forEach((arrayKey) => {
+      arrayKey.addEventListener('click', () => {
+        arrayKey.nextSibling.classList.toggle('u-hide');
+      });
+    });
+  };
 
-
-document.addEventListener('DOMContentLoaded', () => {
-	const convertButton = document.querySelector('button#convert');
+  const convertButton = document.querySelector('button#convert');
   const yamlTextArea = document.querySelector('textarea#yaml');
   const output = document.querySelector('#output');
 	convertButton?.addEventListener('click', () => {
@@ -153,13 +160,9 @@ document.addEventListener('DOMContentLoaded', () => {
     output.innerHTML = table.outerHTML;
     enableCollapsingArrays(output);
   });
+};
 
-  // Find the quiz-csv text area and add a listener for pasting
-  const quizSourceTextArea = document.querySelector('textarea#quiz-source');
-  console.log(quizSourceTextArea);
-  quizSourceTextArea?.addEventListener('paste', handleQuizPaste);
-
-  // Find the reset button and add a listener
-  const resetButton = document.getElementById('reset');
-  resetButton?.addEventListener('click', resetQuiz);
+document.addEventListener('DOMContentLoaded', () => {
+  configureTableDisplay();
+  configureQuiz();
 });
