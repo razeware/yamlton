3e4fa7ce0700135996484ff70116aae6ce02e99e
Sam Davies <sam@kodeco.com>
2023-12-09 11:05:20 +0000

    Adding a copy success message

--- scripts/controllers/clipboard_controller.js
+++ scripts/controllers/clipboard_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "https://unpkg.com/@hotwired/stimulus/dist/stimulus.js";
 
 export default class extends Controller {
-  static targets = [ "source" ];
+  static targets = [ "source", "flash" ];
 
   copy() {
-    window.navigator?.clipboard?.writeText(this.sourceTarget.innerText);
+    window.navigator?.clipboard?.writeText(this.sourceTarget.innerText)?.then(() => {
+      this.flashTarget.classList.remove("is-hidden");
+      setTimeout(() => this.flashTarget.classList.add("is-hidden"), 3000);
+    });
   }
 };
